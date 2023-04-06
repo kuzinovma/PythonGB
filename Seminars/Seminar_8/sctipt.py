29adcc3102426f59efdbfd8881a0dc421db57e7a
kuzinovma <maxkuzinov91@gmail.com>
2023-04-06 15:31:45 +0400

    add hometask 8

--- Seminars/Seminar_8/sctipt.py
+++ Seminars/Seminar_8/sctipt.py
@@ -18,7 +18,7 @@ def input_data():
                     f"Выберите номер варианта: "))
 
     while var != 1 and var != 2:
-        print('Ты дурак?! Даю тебе последний шанс')
+        print('Такого варианта нет!')
         var = int(input("Введите номер варианта: "))
 
     if var == 1:
@@ -30,12 +30,151 @@ def input_data():
 
 
 def print_data():
-    pass
+    print('\nВывожу данные для Вас из 1-ого файла: \n')
+    with open('data_first_variant.csv', 'r', encoding='utf-8') as file:
+        data_first = file.readlines()
+        data_first_v2 = []
+        j = 0
+        for i in range(len(data_first)):
+            if data_first[i] == '\n' or i == len(data_first) - 1:
+                data_first_v2.append(''.join(data_first[j:i + 1]))
+                j = i
+        data_first = data_first_v2
+        print(''.join(data_first))
+        # print(*data_first, sep='')
 
+    print('\nВывожу данные для Вас из 2-ого файла: \n')
+    with open('data_second_variant.csv', 'r+', encoding='utf-8') as file:
+        data_second = file.readlines()
+        data_second_version_second = []
+        j = 0
+        for i in range(len(data_second)):
+            if data_second[i] == '\n' or i == len(data_second)-1:
+                data_second = data_second_version_second.append(''.join(data_second[j:i+1]))
+                j = i
+        data_second = data_second_version_second
+        print(''.join(data_second))
+    return data_first, data_second
 
 def put_data():
-    pass
+    print('Из какого файла Вы хотите изменить данные?')
+    data_first, data_second = print_data()
+    number_file = int(input('Введите номер файла: '))
+
+    while number_file != 1 and number_file != 2:
+        print('Ты дурак?! Даю тебе последний шанс')
+        number_file = int(input('Введите номер файла: '))
+
+    if number_file == 1:  # Можно сделать нумерацию внутри файла
+        print("Какую именно запись по счету Вы хотите изменить?")
+        number_journal = int(input('Введите номер записи: '))
+        # ТУТ НАПИСАТЬ КОД
+        print('Меняем: ', data_first[number_journal-1])
+        with open('data_first_variant.csv', 'r', encoding='UTF-8') as file:
+            data_first = file.readlines()
+
+        if number_journal !=1:
+            start = (number_journal - 1)*5
+        else:
+            start = number_journal - 1
+        print('Что нужно поменять?\n\n'
+              '1. Имя\n '
+              '2. Фамилия\n '
+              '3. Номер\n '
+              '4. Адрекс\n ')
+        command = int(input('Введит номер из списка: '))
+        while command < 1 or command > 4:
+            print('Такого варианта в списке нет!')
+            command = int(input('Введит номер из списка: '))
+
+        if command == 1:
+            data_first[start] = name_data() + '\n'
+        elif command == 2:
+            data_first[start + 1] = surname_data() + '\n'
+        elif command == 3:
+            data_first[start + 2] = phone_data() + '\n'
+        else:
+            data_first[start +3] = address_data() + '\n'
+
+        with open('data_first_variant.csv', 'w', encoding='UTF-8') as file:
+            file.write(''.join(data_first))
+
+    else:
+        print("Какую именно запись по счету Вы хотите изменить?")
+        number_journal = int(input('Введите номер записи: '))
+        # ТУТ НАПИСАТЬ КОД
+        print('Меняем: ', data_second[number_journal - 1])
+        contact = data_second[number_journal - 1].split(','
+        )
+        print('Что нужно поменять?\n\n'
+              '1. Имя\n '
+              '2. Фамилия\n '
+              '3. Номер\n '
+              '4. Адрекс\n ')
+        command = int(input('Введит номер из списка: '))
+
+        while command < 1 or command > 4:
+            print('Такого варианта в списке нет!')
+            command = int(input('Введит номер из списка: '))
+
+        if command == 1:
+            contact[0] = '\n' + name_data()
+        elif command == 2:
+            contact[1] = surname_data()
+        elif command == 3:
+            contact[2] = phone_data()
+        else:
+            contact[3] = address_data() + '\n'
+
+        data_second[number_journal - 1] = ','. join(contact)
+
+        with open('data_second_variant.csv', 'w', encoding='UTF-8') as file:
+            file.write(''.join(data_second))
 
 
 def delete_data():
-    pass
+    print('Из какого файла Вы хотите удалить данные?')
+    data_first, data_second = print_data()
+    number_file = int(input('Введите номер файла: '))
+
+    while number_file != 1 and number_file != 2:
+        print('Такого варианта в списке нет!')
+        number_file = int(input('Введите номер файла: '))
+
+    if number_file == 1:  # Можно сделать нумерацию внутри файла
+        print("Какую именно запись по счету Вы хотите удалить?")
+        number_journal = int(input('Введите номер записи: '))
+        # Можно добавить проверку, чтобы человек не выходил за пределы записи
+        # ТУТ НАПИСАТЬ КОД
+        delete_element = data_first.pop(number_journal -1)
+        print('Удаляем: ', delete_element)
+
+        data_first_v2 = []
+        for item in data_first:
+            if item.startswith('\n'):
+                data_first_v2.append(item[1:])
+            else:
+                data_first_v2.append(item)
+        data_first = data_first_v2        
+
+        with open('data_first_variant.csv', 'w', encoding='UTF-8') as file:
+            file.write(''.join(data_first))
+    
+    else:
+        print("Какую именно запись по счету Вы хотите удалить?")
+        number_journal = int(input('Введите номер записи: '))
+        # Можно добавить проверку, чтобы человек не выходил за пределы записи
+        # ТУТ НАПИСАТЬ КОД
+        delete_element = data_second.pop(number_journal - 1)
+        print('Удаляем', delete_element)
+
+        data_second_v2 = []
+        for item in data_second:
+            if item.startswith('\n'):
+                data_second_v2.append(item[1:])
+            else:
+                data_second_v2.append(item)
+        data_second = data_second_v2
+
+        with open('data_second_variant,csv', 'w', encoding='UTF-8') as file:
+            file.write(''.join(data_second))
